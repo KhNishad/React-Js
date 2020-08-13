b648cfb2bd1878d7836078dfc1a49914e5a91bb5
khNishad <kkhnishad@gmail.com>
2020-08-13 17:44:51 +0600

    hello world

---
+++ src/components/greet.js
@@ -0,0 +1,11 @@
+// functional component exp
+
+import React from 'react'
+// function Greet() {
+//     return <h1>Hello Nishad</h1> 
+    
+// }
+
+// arrow function
+const Greet = () => <h1>Hello Nishad</h1> 
+export default Greet;
